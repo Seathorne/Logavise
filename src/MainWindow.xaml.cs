eaccedf4144e0ab60ad31a15c92e52a09e6de0cd
Seathorne <scottketclarke@gmail.com>
2025-06-14 03:53:08 -0400

    Generated Visual Studio project
    Default VisualStudio project with addition of src/ and ui/ directories.

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Logavise
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
