ca296aafbadb4ca1e7783539180df9f8fe973bb5
Seathorne <scottketclarke@gmail.com>
2025-06-14 05:23:38 -0400

    Added RegEx matching for defined fields
    Paste text into the editor and the fields will automatically update if the RegEx pattern matches the currently selected line.

---
+++ model/RegexField.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace Logavise
+{
+    public class RegexField : INotifyPropertyChanged
+    {
+        private string _pattern;
+        private string _fieldName;
+        private string _value;
+
+        public string Pattern
+        {
+            get => _pattern;
+            set
+            {
+                _pattern = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FieldName
+        {
+            get => _fieldName;
+            set
+            {
+                _fieldName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Evaluate(string value)
+        {
+            var match = Regex.Match(value, Pattern);
+            if (match.Success)
+            {
+                Value = match.Groups[1].Value;
+            }
+            else Value = null;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,12 +1,74 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace Logavise
 {
     public partial class MainWindow : Window
     {
+        private int previousLineNumber;
+
+        public ObservableCollection<RegexField> RegexFields { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this; // For XAML binding to work
+
+            textEditor.AppendText("00:19:23.763 [1] (null) INFO  Equipment 1 - 00:19:23:759 - Update Printer Receiving Line 1 PandA 1 Enabled False using Tag: SCANNER_30_PRINTER_W_STATUS[1]");
+
+            RegexFields = new ObservableCollection<RegexField>
+            {
+                new RegexField  // 11:54:58:379
+                {
+                    Pattern = @"^(\d{2}:\d{2}:\d{2}\.\d{3})",
+                    FieldName = "Timestamped Message"
+                },
+
+                new RegexField  // 195
+                {
+                    Pattern = @"\[(\d+)\]",
+                    FieldName = "Thread Number"
+                },
+
+                new RegexField  // INFO
+                {
+                    Pattern = @"(INFO|DEBUG|ERROR)",
+                    FieldName = "Level"
+                },
+
+                new RegexField  // Equipment 5
+                {
+                    Pattern = @"Equipment (\d+)",
+                    FieldName = "Equipment Number"
+                },
+            };
+
+            textEditor.TextArea.Caret.PositionChanged += Caret_PositionChanged;
         }
+
+        #region Event Handlers
+
+        private void Caret_PositionChanged(object sender, EventArgs e)
+        {
+            int lineNumber = textEditor.TextArea.Caret.Line;
+
+            if (lineNumber != previousLineNumber)
+            {
+                var currentLine = textEditor.Document.GetLineByNumber(lineNumber);
+                string lineText = textEditor.Document.GetText(currentLine);
+
+                foreach (var regex in RegexFields)
+                {
+                    regex.Evaluate(lineText);
+                }
+
+                previousLineNumber = lineNumber;
+            }
+        }
+
+        #endregion
     }
 }
+
+// 00:19:23.763 [1] (null) INFO  Equipment 1 - 00:19:23:759 - Update Printer Receiving Line 1 PandA 1 Enabled False using Tag: SCANNER_30_PRINTER_W_STATUS[1]
